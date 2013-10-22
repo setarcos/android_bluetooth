d0f60527e97f1dcf4a77476160d23d5f04b957ab
YanJun Yang <yangyj.ee@gmail.com>
2013-10-22 17:05:50 +0800

    Bluetooth send msg to the UART done

--- src/com/pluto/myfirstapp/MainActivity.java
+++ src/com/pluto/myfirstapp/MainActivity.java
@@ -1,13 +1,32 @@
 package com.pluto.myfirstapp;
 
+import java.util.Set;
+import java.util.UUID;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import android.util.Log;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.content.Intent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 
 public class MainActivity extends Activity
 {
+    private BluetoothAdapter btAdapter = null;
+    private BluetoothSocket btSocket = null;
+    private BluetoothDevice btDevice = null;
+    private static final int REQUEST_ENABLE_BT = 1;
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private ConnectedThread mConnectedThread;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -16,10 +35,118 @@ public class MainActivity extends Activity
         setContentView(R.layout.main);
     }
 
-    public void buttonClick(View view) {
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if ((requestCode == REQUEST_ENABLE_BT) && (resultCode != RESULT_OK))
+            errorExit("Fatal Error", "蓝牙必须打开");
+    }
+
+    public void connClick(View view) {
+        TextView viewMsg = (TextView) findViewById(R.id.view_message);
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        checkBTState();
+
+        Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
+                viewMsg.append('\n' + device.getName()); // device.getAddress()
+                if (device.getName().indexOf("HC-05") >= 0)
+                    btDevice = device;
+            }
+        }
+        if (btDevice != null) {
+            mConnectedThread = new ConnectedThread(btDevice);
+            mConnectedThread.start();
+        } else {
+            errorExit("Fatal Error", "没有找到蓝牙串口");
+        }
+    }
+
+    public void sendClick(View view) {
         EditText editText = (EditText) findViewById(R.id.edit_message);
         String message = editText.getText().toString();
-        TextView viewMsg = (TextView) findViewById(R.id.view_message);
-        viewMsg.setText(message);
+        if (mConnectedThread != null)
+            mConnectedThread.write(message);
+    }
+
+    private void checkBTState() {
+    // Check for Bluetooth support and then check to make sure it is turned on
+        if(btAdapter==null) {
+            errorExit("Fatal Error", "不支持蓝牙");
+        } else {
+            if (!btAdapter.isEnabled()) {
+                //Prompt user to turn on Bluetooth
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+    }
+
+    private void errorExit(String title, String message){
+        Toast.makeText(getBaseContext(), title + " - " + message, Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+    private class ConnectedThread extends Thread {
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private final BluetoothSocket mmSocket;
+
+        public ConnectedThread(BluetoothDevice device) {
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+            BluetoothSocket tmp = null;
+            try {
+                // using temp objects because member streams are final
+                tmp = device.createRfcommSocketToServiceRecord(MY_UUID);
+                tmpIn = tmp.getInputStream();
+                tmpOut = tmp.getOutputStream();
+            } catch (IOException e) { }
+            mmSocket = tmp;
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[256];  // buffer store for the stream
+            int bytes; // bytes returned from read()
+            btAdapter.cancelDiscovery();
+            try {
+                // until it succeeds or throws an exception
+                mmSocket.connect();
+            } catch (IOException connectException) {
+                // Unable to connect; close the socket and get out
+                try {
+                    mmSocket.close();
+                } catch (IOException closeException) { }
+                return;
+            }
+            Log.d("MYBT", "connected");
+
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                try {
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);        // Get number of bytes and message in "buffer"
+                    Log.d("MYBT", "got something");
+                //    h.obtainMessage(RECIEVE_MESSAGE, bytes, -1, buffer).sendToTarget();        // Send to message queue Handler
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(String message) {
+            byte[] msgBuffer = message.getBytes();
+            try {
+                mmOutStream.write(msgBuffer);
+                Log.d("MYBT", "write msg");
+            } catch (IOException e) {
+                Log.d("MYBT", "...Error data send: " + e.getMessage() + "...");
+            }
+        }
     }
 }
